380a6845e7af4dc8d0bb4d391d070966e466c201
Toàn Trần <neo290992@gmail.com>
2021-06-08 10:27:36 +0700

    tạo

--- my.js
+++ my.js
@@ -4,7 +4,11 @@ let num = 2
 
 for(let i = 0;i<= n-1;i++){
     num = num + i;
-    document.write(num+" ");
+   if((i+1)%5==0){
+       document.write(num+"<br>");}
+    else{
+        document.write(num+" ");
+    }
     
     
 }
